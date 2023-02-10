fb228449756fc6a0f35c81ab5d018bd510a86ad5
Jessie Bang <jessiebang7@gmail.com>
2023-02-09 21:15:08 -0500

    Remove dummy expenses data

--- src/app.js
+++ src/app.js
@@ -3,27 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+// import { addExpense } from './actions/expenses';
+// import { setTextFilter } from './actions/filters';
+// import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-  console.log(visibleExpenses);
-});
-
-
-store.dispatch(addExpense({ description: 'Water Bill', note: 'Feb 2023 - Water Bill', amount: 4500, createdAt: -100000000}));
-store.dispatch(addExpense({ description: 'Gas Bill', note: 'Feb 2023 - Gas Bill', amount: 0, createdAt: 100000000}));
-store.dispatch(addExpense({ description: 'Rent', note: 'Feb 2023 - Water Bill', amount: 109500, createdAt: 0}));
-
-// store.dispatch(setTextFilter('water'));
-// setTimeout(()=> { store.dispatch(setTextFilter('bill')); }, 3000);
 
 const jsx = (
   <Provider store={store}>
@@ -33,6 +20,17 @@ const jsx = (
 
 ReactDOM.render(jsx, document.getElementById('app'));
 
+// store.subscribe(() => {
+//   const state = store.getState();
+//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//   console.log(visibleExpenses);
+// });
+
+// store.dispatch(addExpense({ description: 'Water Bill', note: 'Feb 2023 - Water Bill', amount: 4500, createdAt: -100000000}));
+// store.dispatch(addExpense({ description: 'Gas Bill', note: 'Feb 2023 - Gas Bill', amount: 0, createdAt: 100000000}));
+// store.dispatch(addExpense({ description: 'Rent', note: 'Feb 2023 - Water Bill', amount: 109500, createdAt: 0}));
+// store.dispatch(setTextFilter('water'));
+// setTimeout(()=> { store.dispatch(setTextFilter('bill')); }, 3000);
 
 /* 
 const demoState = {
